35dcf8b43654b4c943bd3975bb1b06367f7d1e2b
Shrirang Mare <sbmare@me.com>
2017-08-14 13:28:20 -0700

    Fix SMS syncing
    SMS messages now get added, updated, and deleted.
    MMS messages also get synced but all MMS message metadata is not synced, not yet.

--- QKSMS/src/withAnalytics/java/com/moez/QKSMS/QKSMSApp.java
+++ QKSMS/src/withAnalytics/java/com/moez/QKSMS/QKSMSApp.java
@@ -1,12 +1,36 @@
 package com.moez.QKSMS;
 
+import android.util.Log;
+
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 
 public class QKSMSApp extends QKSMSAppBase {
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+
+
+    }
+
+    private class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+
+            if (priority == Log.WARN || priority == Log.ERROR) {
+                Crashlytics.getInstance().core.log(priority, tag, message);
+            }
+            if (t != null) {
+                Crashlytics.getInstance().core.logException(t);
+            }
+        }
     }
 }
